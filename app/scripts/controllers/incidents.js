c2d80b3411e91007ce9da3659bcaa7c0dd1df31f
antonyjsmith <antonysmith@me.com>
2015-09-18 14:22:04 +0100

    Removed SVG to support IE11
    Amended map loading ordered too

--- app/scripts/controllers/incidents.js
+++ app/scripts/controllers/incidents.js
@@ -3,23 +3,23 @@
  * @ngdoc function
  * @name sbAdminApp.controller:
  * @description
- * # 
+ * #
  * Controller of the sbAdminApp
  */
  angular.module('incidents', [])
 
 	.controller('incidentFeedController', ['$scope', '$http', '$filter', '$routeParams', 'wpPosts', 'wpPostsRating', 'localStorageService',
 	  function($scope, $http, $filter, $routeParams, wpPosts, wpPostsRating, localStorageService) {
-		  
+
 	  	  $scope.itemsPerPage = 10;
-	  	  	  	  
+
 		  //$scope.counts = wpPosts.query({postCount: $scope.itemsPerPage}, function(response){
-			
+
 			//$scope.totalItems = response.meta.count;
-						  
+
 		  //});
-		  
-		  
+
+
 		var listQuery = {"params": [
 			        {
 			            "name": "offset",
@@ -37,67 +37,67 @@
 			            "type": "integer"
 			        }
 		]};
-				  
+
 		$scope.incidents = wpPostsRating.query(listQuery, function(response){
-			
+
 			$scope.totalItems = response.count;
-						
-		}); 
-		  
-		  		 
-				        		  
+
+		});
+
+
+
 		  $scope.currOffset = 0;
-		  
+
 		  $scope.currShowing = $scope.currOffset + $scope.itemsPerPage;
-		  		  
+
 		  $scope.currentPage = 1;
-		  
+
 		  $scope.clear = function(){
-			  
+
 			  $scope.search = '';
 			  localStorageService.set('incident_search_string', null);
 			  $scope.pageChanged($scope.search);
-			  
+
 		  }
-		
+
 		  $scope.pageChanged = function(searchValue) {
-			  
+
 		  	function submit(val) {
-			  	
+
 		  		return localStorageService.set('incident_search_string', val);
-		  		
-			}  
-			  
+
+			}
+
 		    console.log('Page changed to: ' + $scope.currentPage);
 		    console.log('Offset ' + ($scope.currentPage - 1) * $scope.itemsPerPage);
-		    
+
 		    var searchString = '%%';
-		    
+
 		    var checkSearchValue = function (val) {
-			    
+
 			    if ((typeof val == 'undefined') || (val == null)) {
-				    
+
 				    console.log('nothing there');
-				    
+
 			    } else {
-				    
+
 				    searchString = '%'+val+'%';
-				    
+
 			    }
-			    
+
 		    };
-		    
+
 		    submit(searchValue);
 		    checkSearchValue(searchValue);
-		    
+
 		    $scope.currOffset = ($scope.currentPage - 1) * $scope.itemsPerPage;
-		    			        
+
 			      //$scope.incidents = data;
 			      //$scope.totalItems = data.meta.count;
-			      
+
 			      $scope.currShowing = $scope.currOffset + $scope.itemsPerPage;
 
-			    
+
 				var listQuery = {"params": [
 			        {
 			            "name": "offset",
@@ -115,56 +115,56 @@
 			            "type": "integer"
 			        }
 				]};
-						  
+
 				$scope.incidents = wpPostsRating.query(listQuery, function(response){
-					
+
 					$scope.totalItems = response.count;
-								
-				}); 
-			    
-			    
+
+				});
+
+
 		  };
-		
+
 		  $scope.maxSize = 5;
-		  
-		  
+
+
 		  if(localStorageService.isSupported) {
-			  		    
+
 		    $scope.search = localStorageService.get('incident_search_string');
-		    
+
 		    if ($scope.search != null) {
-			    
+
 			    $scope.pageChanged($scope.search);
-			    
+
 		    }
-		    		    
+
 		  }
- 
-		} 	  
+
+		}
 	])
-	
+
 	.controller('incidentDetailCtrl', ['$scope', '$stateParams', 'Incident', 'uiGmapGoogleMapApi',
 	  function($scope, $stateParams, Incident, uiGmapGoogleMapApi) {
-		  	  
+
 	  		$scope.incident = Incident.query({incidentID: $stateParams.incidentID});
-	  		
+
 	  		$scope.incident.$promise.then(function (location){
-		  		
+
 		  		//set variables and convert string to integer
 		  		var lat = parseFloat(location.location_data.incident_latitude);
 		  		var lng = parseFloat(location.location_data.incident_longitude);
-		  		
-		  		$scope.map = { center: { latitude: lat, longitude: lng }, zoom: 8 };
-		  		$scope.point = { latitude: lat, longitude: lng };
-		  				  		
-	  		})
-	  		
-			uiGmapGoogleMapApi.then(function(maps) {
-				$scope.map = { center: { latitude: 0, longitude: 0 }, zoom: 2 };
-			});
-					  		
 
-		}	  
+		  		$scope.map = { center: { latitude: lat, longitude: lng }, zoom: 8 };
+          $scope.key = parseFloat(location.ID);
+		  		$scope.point = { latitude: lat, longitude: lng };
+
+    			uiGmapGoogleMapApi.then(function(maps) {
+    				$scope.map = { center: { latitude: 0, longitude: 0 }, zoom: 2 };
+    			});
+
+	  		})
+
+		}
 	])
-	
-	;	
+
+	;
--- app/scripts/controllers/maps.js
+++ app/scripts/controllers/maps.js
@@ -1,15 +1,15 @@
 'use strict';
-  angular.module('maps', [])	
-	
+  angular.module('maps', [])
+
 	.controller('incidentMapController', ['$scope', '$http', '$timeout', '$location', 'Incidents', 'CountryList', 'geoIncident', 'uiGmapGoogleMapApi', 'modalService', 'Restangular',
 	  function($scope, $http, $timeout, $location, Incidents, CountryList, geoIncident, uiGmapGoogleMapApi, modalService, Restangular) {
-		  		  		  		  		  	
+
             var evacPolyProperties= {
-	                    
+
 	                    "0": {
 	                            name: 'Monitoring',
 	                            colors: [ '#006699' ]
-	                    },            
+	                    },
 	                    "1": {
 	                            name: 'Increased Monitoring',
 	                            colors: [ '#00CC00' ]
@@ -26,11 +26,11 @@
 	                            name: 'Evacuation',
 	                            colors: [ '#FF0000' ]
 	                    },
-                    
+
                    };
-                   
+
             var riskPolyProperties= {
-	                               
+
 	                    "1": {
 	                            name: 'Low',
 	                            colors: [ '#3BAC54' ]
@@ -47,23 +47,23 @@
 	                            name: 'Extreme',
 	                            colors: [ '#FF0000' ]
 	                    },
-                    
+
                    };
-                   
+
 			$scope.setSearch	= function(bool){
-				
+
 					$scope.circle.visible = bool;
-				
+
 				};
-                   
-            $scope.incidentParams 				= new Object({});
-		  	$scope.incidentParams.count 		= 200;
+
+            $scope.incidentParams 				    = new Object({});
+		  	    $scope.incidentParams.count       = 200;
             $scope.incidentParams.dateBefore 	= moment().format("YYYY-MM-DD");
             $scope.incidentParams.dateAfter		= moment().subtract(30, 'days').format("YYYY-MM-DD");
-                   
+
             $scope.loading = false;
-            
-            //Open or close datepicker			
+
+            //Open or close datepicker
 			$scope.calendar = {
 			    opened: {},
 			    dateFormat: 'MM/dd/yyyy',
@@ -72,90 +72,159 @@
 			        $event.preventDefault();
 			        $event.stopPropagation();
 			        $scope.calendar.opened[which] = !$scope.calendar.opened[which];
-			    } 
+			    }
 			};
-			                     
+
 		  	$scope.markers = [];
-		  	
+
 		  	//Set max date in date picker
-		  	
-		  	$scope.maxDate = new Date();	
-		  	
+
+		  	$scope.maxDate = new Date();
+
 		  	var populateMarkerModel = function(input){
-			  	
+
 			  		//$scope.markers.length = 0;
-			  		
+
 			  		if (input.length == 0){
-				  	
+
 					  	var modalOptions = {
 				            actionButtonText: 'Ok',
 				            headerText: 'No results',
 				            bodyText: 'Try broadening your search parameters'
 				        };
-				
-				        modalService.showModal({}, modalOptions);	
-				  		
+
+				        modalService.showModal({}, modalOptions);
+
 			  		}
-			  		
+
 					_.each(input, function (incident){
-						
+
 						//set variables and convert string to integer
 						var lat = parseFloat(incident.location_data.incident_latitude);
 						var lng = parseFloat(incident.location_data.incident_longitude);
-						
+
 						var point = {
-							
+
 								latitude: lat,
 								longitude: lng,
 								id: incident.ID,
 								title: '<b><p>' + incident.title + ' - Incident Rating: ' + incident.rating_data.incident_rating + '</p></b>' + incident.excerpt + '<a href="/#/portal/incident/feed/' + incident.ID + '">More...</a>',
-								icon: "assets/level-" + incident.rating_data.incident_rating + ".svg"
-							
+								icon: "assets/level-" + incident.rating_data.incident_rating + ".png"
+
 							};
-						
+
 						$scope.markers.push(point);
-																	
+
 					});
-					
+
 					$scope.loading = false;
-					
-					//console.log($scope.markers.length);			  	
-			  	
+
+					//console.log($scope.markers.length);
+
 		  	};
-		  	
+
+      var drawMap = function(){
+
+        uiGmapGoogleMapApi.then(function(maps) {
+
+          $scope.map = {
+                center: {
+                  latitude: 40.74,
+                  longitude:  -16.18
+                },
+                pan: true,
+                zoom: 2,
+                visible: false,
+                events: {},
+                bounds: {},
+                polys: [],
+                polyFill: function(model){
+                        if(!model){
+                          console.log("model undefined!");
+                          return;
+                        }
+                        polyFillCtr += 1;
+                        //console.log("polyFillCtrRisk: " + polyFillCtr + ", id: " + model.id);
+                        return { color: model.rating, opacity: '0.6' };
+                      },
+                polyEvents: {
+                  click: function (gPoly, eventName, polyModel) {
+                    //window.alert("Poly Clicked: id:" + polyModel.id + ' ' + JSON.stringify(polyModel.path));
+                    console.log(polyModel.id);
+                      var resource = Restangular.all('rest/ml-sql/globalwatch_country')
+
+              resource.customGET(["?filter=countryISO3%20%3D%20'" +polyModel.id+ "'"]).then(function(response){
+                $location.path('/portal/countries/overview/' +response.record[0].countryID);
+              });
+                  }
+                }
+              };
+
+              $scope.map.polys = paths.features;
+
+          $scope.map2 = {
+                visible: false,
+                events: {},
+                bounds: {},
+                polys: [],
+                polyFill: function(model){
+                        if(!model){
+                          console.log("model undefined!");
+                          return;
+                        }
+                        polyFillCtr += 1;
+                        //console.log("polyFillCtrEvac: " + polyFillCtr + ", id: " + model.id);
+                        return { color: model.evacrating, opacity: '0.6' };
+
+                      },
+                polyEvents: {
+                  click: function (gPoly, eventName, polyModel) {
+                    //window.alert("Poly Clicked: id:" + polyModel.id + ' ' + JSON.stringify(polyModel.path));
+                    console.log(polyModel);
+                  }
+                }
+              };
+
+          $scope.map2.polys = paths.features;
+
+        });
+
+      };
+
 			$scope.getIncidents = function (){
-				
+
 				$scope.incident = Incidents.query();
-			
+
 				$scope.incident.$promise.then(function (location){
-					
+
 					populateMarkerModel(location);
-																	
+          drawMap();
+
 				})
-				
+
 			};
-			
+
 			$scope.getIncidents();
-	  
-						
+
+
 			$scope.updateIncidents = function (){
-				
+
 				var fromDate = moment($scope.incidentParams.dateBefore).format("YYYY-MM-DD");
 				var toDate = moment($scope.incidentParams.dateAfter).format("YYYY-MM-DD");
-								
+
 				$scope.loading = true;
-				
+
 				$scope.incident = Incidents.query({postCount:$scope.incidentParams.count, dateBefore:fromDate, dateAfter:toDate});
-			
+
 				$scope.incident.$promise.then(function (location){
-					
+
 					populateMarkerModel(location);
-																	
+
 				})
-				
+
 			};
-						
-			
+
+
 			  $scope.markersEvents = {
 			    click: function (gMarker, eventName, model) {
 			      //console.log(model);
@@ -163,35 +232,35 @@
 			      model.show = !model.show;
 			    }
 			  };
-			  
-			
-			
+
+
+
 			var paths = new Object({});
 			paths.type = "FeatureCollection";
 			paths.features = [];
 			var polyFillCtr = 0;
-			
+
 		  	var countryData = CountryList.query();
-		  	
+
 		  	countryData.$promise.then(function (response){
-			  	
+
 			  	$scope.countries = {};
-			  				  	
+
                 for (var i=0; i< response.length; i++) {
                     var country = response[i];
                     $scope.countries[country['countryISO3']] = country;
                 };
-                
+
 	            function getColor(country) {
 	                if (!country || !country["countryRating"]) {
 	                    return "#FFF";
 	                }
-	
+
 	                var colors = riskPolyProperties[country["countryRating"]].colors;
 	                var index = country["countryISO3"].charCodeAt(0) % colors.length ;
 	                return colors[index];
 	            }
-	            
+
 	            function getColorEvac(country) {
 					if (typeof country == 'undefined') {
 						return "#FFF";
@@ -206,20 +275,20 @@
 						var index = country["countryISO3"].charCodeAt(0) % colors.length ;
 						return colors[index];
 					}
-	               
+
 	            }
-	                            			  				  	
+
 				var polygon = $http.get('data/geo-json/countries.geo.json').success(function(response) {
-					
+
 					_.each(response.features, function(borders){
-																						
+
 						if (borders.geometry.type === 'Polygon'){
-													
+
 							_.each(borders.geometry.coordinates, function(array){
-									
+
 								var nest						= [];
 								nest.push(array);
-																																					
+
 								var point						= new Object({});
 								point['id']						= borders.id;
 								point['geometry']				= new Object({});
@@ -227,16 +296,16 @@
 								point.geometry['type'] 			= "Polygon";
 								point['rating']					= getColor($scope.countries[borders.id]);
 								point['evacrating']				= getColorEvac($scope.countries[borders.id]);
-								
+
 								paths.features.push(point)
-									
+
 							})
-							
-							
+
+
 						} else if (borders.geometry.type === 'MultiPolygon'){
-																								
+
 							_.each(borders.geometry.coordinates, function(array){
-							
+
 								var point						= new Object({});
 								point['id']						= borders.id;
 								point['geometry']				= new Object({});
@@ -244,117 +313,53 @@
 								point.geometry['type'] 			= "Polygon";
 								point['rating']					= getColor($scope.countries[borders.id]);
 								point['evacrating']				= getColorEvac($scope.countries[borders.id]);
-																		
+
 								paths.features.push(point)
-							
+
 							})
-						
+
 						};
-						
+
 					});
-					
+
 				});
-			  	
+
 			});
-			
+
 			//Far from the most elegant method, due to a bug in uiGmapGoogleMapApi visibility cannot be bound to a model so this a nasty workaround
-		  	  	
+
 		  	$scope.layerModel = 0;
 		  	$scope.threatLegend = false;
 			$scope.evacLegend = false;
-		  			
+
 		  	$scope.switchLayer = function (layer){
-		  		
+
 		  		if (layer == 0) {
-			  		
+
 			  		$scope.map.visible = false;
 			  		$scope.map2.visible = false;
 			  		$scope.threatLegend = false;
 			  		$scope.evacLegend = false;
-			  		
+
 		  		} else if (layer == 1) {
-			  		
+
 			  		$scope.map.visible = true;
 			  		$scope.map2.visible = false;
 			  		$scope.evacLegend = true;
 			  		$scope.threatLegend = false;
-			  		
+
 		  		} else if (layer == 2) {
-			  	
+
 			  		$scope.map.visible = false;
 			  		$scope.map2.visible = true;
 			  		$scope.evacLegend = false;
 			  		$scope.threatLegend = true;
-			  		
+
 		  		};
-			  	
-			  	
+
+
 		  	};
-		  			
-			uiGmapGoogleMapApi.then(function(maps) {
-																	
-				$scope.map = {
-		          center: {
-		            latitude: 40.74,
-		            longitude:  -16.18
-		          },
-		          pan: true,
-		          zoom: 2,
-		          visible: false,
-		          events: {},
-		          bounds: {},
-		          polys: [],
-		          polyFill: function(model){
-					            if(!model){
-					              console.log("model undefined!");
-					              return;
-					            }
-					            polyFillCtr += 1;
-					            //console.log("polyFillCtrRisk: " + polyFillCtr + ", id: " + model.id);
-					            return { color: model.rating, opacity: '0.6' };
-					          },
-		          polyEvents: {
-		            click: function (gPoly, eventName, polyModel) {
-		              //window.alert("Poly Clicked: id:" + polyModel.id + ' ' + JSON.stringify(polyModel.path));
-		              console.log(polyModel.id);
-		                var resource = Restangular.all('rest/ml-sql/globalwatch_country')
-				
-						resource.customGET(["?filter=countryISO3%20%3D%20'" +polyModel.id+ "'"]).then(function(response){
-							$location.path('/portal/countries/overview/' +response.record[0].countryID);
-						});
-		            }
-		          }
-		        };
-		        
-		        $scope.map.polys = paths.features;
-		        
-				$scope.map2 = {
-		          visible: false,
-		          events: {},
-		          bounds: {},
-		          polys: [],
-		          polyFill: function(model){
-					            if(!model){
-					              console.log("model undefined!");
-					              return;
-					            }
-					            polyFillCtr += 1;
-					            //console.log("polyFillCtrEvac: " + polyFillCtr + ", id: " + model.id);
-					            return { color: model.evacrating, opacity: '0.6' };
-					            
-					          },
-		          polyEvents: {
-		            click: function (gPoly, eventName, polyModel) {
-		              //window.alert("Poly Clicked: id:" + polyModel.id + ' ' + JSON.stringify(polyModel.path));
-		              console.log(polyModel);
-		            }
-		          }
-		        };			 
-		               
-				$scope.map2.polys = paths.features;
-									
-			});
-			
+
 	        $scope.circle =
 	            {
 	                center: {
@@ -383,19 +388,19 @@
 			            radius_changed: function (gCircle, eventName, circleModel) {
 				          $scope.circle.radius = circleModel.radius;
 			              console.log(circleModel);
-			            },		                
+			            },
 	                },
 	                control: {}
 	            };
-	        	        
-				
+
+
 			$scope.updateGeoIncidents = function (){
-				
+
 				var fromDate = moment($scope.incidentParams.dateBefore);
 				var toDate = moment($scope.incidentParams.dateAfter);
-				
+
 				$scope.loading = true;
-				
+
 				var locQuery = {"params": [
 						        {
 						            "name": "lat",
@@ -429,109 +434,109 @@
 						        }
 						    ]
 						  };
-						        
+
 				var geo = geoIncident.query(locQuery);
-				
+
 				geo.$promise.then(function (response){
-															
+
 					populateGeoMarkerModel(response);
-					
+
 				});
-			
+
 			};
-			
+
 			function getMiles(i) {
 			     return i*0.000621371192;
 			}
-			
+
 		  	var populateGeoMarkerModel = function(input){
-			  	
+
 			  		if (input.length == 0){
-				  	
+
 					  	var modalOptions = {
 				            actionButtonText: 'Ok',
 				            headerText: 'No results',
 				            bodyText: 'Try broadening your search parameters'
 				        };
-				
-				        modalService.showModal({}, modalOptions);	
-				  		
+
+				        modalService.showModal({}, modalOptions);
+
 			  		}
-			  				  	
+
 			  		$scope.markers.length = 0;
-			  					  		
+
 			  		//set a small delay to make sure the map is cleared first [evalAsync doesn't work for some reason]
 			  		$timeout(function () {
-				  						  		
+
 						_.each(input, function (incident){
-														
+
 							//set variables and convert string to integer
 							var lat = parseFloat(incident.incident_latitude);
 							var lng = parseFloat(incident.incident_longitude);
-							
+
 							var getRating = function(rating){
-								
+
 								if (rating == null) {
-									
+
 									console.log('Catch me');
 									return 1;
-									
+
 								} else {
-									
+
 									return rating;
-									
+
 								};
-								
+
 							};
-							
+
 							var point = {
-								
+
 									latitude: lat,
 									longitude: lng,
 									id: incident.ID,
 									title: '<b><p>' + incident.title + ' - Incident Rating: ' + incident.incident_rating + '</p></b><p>' + incident.excerpt + '</p><a href="/#/portal/incident/feed/' + incident.ID + '">More...</a>',
 									icon: "assets/level-" + getRating(incident.incident_rating) + ".svg"
-								
+
 								};
-							
+
 							$scope.markers.push(point);
-																		
+
 						});
-						
+
 						$scope.loading = false;
-						
+
 					}, 250);
-					
-					
-					//console.log($scope.markers.length);			  	
-			  	
+
+
+					//console.log($scope.markers.length);
+
 		  	};
-		  	
+
 		  	$scope.clearMarkers = function(){
-			  	
+
 			  	$scope.markers.length = 0;
-			  	
-		  	};	       						
-					  
+
+		  	};
+
 	  }])
-	  
+
     .controller('legendCtrl', ['$scope', 'Restangular',
 		function($scope, Restangular) {
 
 			var resourceRatings = Restangular.all('rest/db/rating_terms')
-					  			  					
+
 			resourceRatings.customGET(["?filter=type='evac_level'"]).then(function(data){
-				
+
 				$scope.evacData = data.record;
-						  		
+
 			});
-					  			  			  					
+
 			resourceRatings.customGET(["?filter=type='risk_level'"]).then(function(data){
-					
+
 				$scope.threatData = data.record;
-							  		
+
 	  		});
 
     }])
-	  
+
 	;
